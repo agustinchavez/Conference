1832f11e223846a3e17eed044e00549ad7b2ae87
agustinchavez <chavez13@illinois.edu>
2017-08-12 13:02:54 -0500

    * Web.config: * packages.config: * ConferenceContext.cs: * ConferenceContextInitializer.cs:
    * Conference.csproj: added entity framework conference context and
      initializer as go between for db

---
+++ Conference/Models/ConferenceContextInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Conference.Models
+{
+    public class ConferenceContextInitializer : DropCreateDatabaseAlways<ConferenceContext>
+    {
+        public override void Seed(ConferenceContext context)
+        {
+            context.Sessions.Add(
+                new Session()
+                {
+                    Title = "I want tacos",
+                    Abstract = "The life and times of a taco lover",
+                    Speaker = context.Speakers.Add(
+                    new Speaker()
+                    {
+                        Name = "Agustin",
+                        EmailAddress = "agustin@agustin.com"
+                    })
+                });
+        }
+    }
+}
